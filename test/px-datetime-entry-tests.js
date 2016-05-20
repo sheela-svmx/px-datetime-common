008e37c556193ddbb26f5adf5eb734b1eb91b2b5
Benoit Chevalier <benoit.chevalier@ge.com>
2016-05-20 10:03:34 -0700

    tests

--- test/px-datetime-entry-tests.js
+++ test/px-datetime-entry-tests.js
@@ -23,22 +23,85 @@ function runCustomTests() {
   // Use testCase(options) for a more convenient setup of the test cases
   suite('Custom Automation Tests for px-datetime-entry', function() {
 
-    test('cell selection', function(done) {
-      var cells = Polymer.dom(date1.root).querySelectorAll('px-datetime-entry-cell'),
-          firstInput = Polymer.dom(cells[0].root).querySelector('input');
+    test('cell selection', function() {
+      var cells = Polymer.dom(date1.root).querySelectorAll('px-datetime-entry-cell');
 
-      var clicked = function(event) {
+      //simulate focus on first cell..............................................
+      cells[0]._handleFocus();
 
-        assert.isTrue(date1.isSelected);
-        assert.isTrue(cells[0].isSelected);
-        assert.isFalse(cells[1].isSelected);
-        done();
-      }
+      assert.isTrue(date1.isSelected);
+      assert.isTrue(cells[0].isSelected);
+      assert.isFalse(cells[1].isSelected);
 
-      //hook event handler on first cell
-      firstInput.addEventListener('click', clicked);
-      firstInput.click();
     });
 
+    test('cell keyboard up and down', function() {
+      var cells = Polymer.dom(date1.root).querySelectorAll('px-datetime-entry-cell'),
+          currentYear = parseInt(cells[0].dtWorkingCopy);
+
+      fireKeyboardEvent(cells[0], 'ArrowUp');
+      assert.equal(currentYear + 1, parseInt(cells[0].dtWorkingCopy))
+      fireKeyboardEvent(cells[0], 'ArrowDown');
+      assert.equal(currentYear , parseInt(cells[0].dtWorkingCopy))
+    });
+
+    test('cell keyboard autocomplete onleaving', function(done) {
+      var cells = Polymer.dom(date1.root).querySelectorAll('px-datetime-entry-cell'),
+          firstInput = Polymer.dom(cells[0].root).querySelector('input'),
+          currentYear = parseInt(cells[0].dtWorkingCopy);
+
+      fireKeyboardEvent(cells[0], '2');
+      fireKeyboardEvent(cells[0], '2');
+
+      //entering 2 characters + arrow right should autocomplete + move to the next cell.
+      //Unfortunately our code relies on focus() which doesn't seem to
+      //work in the testing environment. Instead listen to the event that will result
+      //in the cell to be changed
+      cells[0].addEventListener('px-entry-cell-move', function(evt) {
+
+        //fake the moving ourselves.............
+        cells[0]._handleBlur();
+debugger;
+        //make sure autocomplete happened
+        assert.equal(cells[0].dtWorkingCopy, 2022);
+        assert.equal(firstInput.value, 2022);
+        done();
+      })
+      fireKeyboardEvent(cells[0], 'ArrowRight');
+    });
+
+    test('auto move to next cell', function(done) {
+      var cells = Polymer.dom(date1.root).querySelectorAll('px-datetime-entry-cell'),
+          firstInput = Polymer.dom(cells[0].root).querySelector('input'),
+          currentYear = parseInt(cells[0].dtWorkingCopy);
+
+
+      //simulate focus on first cell..............................................
+      cells[0]._handleFocus();
+      fireKeyboardEvent(cells[0], '2');
+      fireKeyboardEvent(cells[0], '2');
+      fireKeyboardEvent(cells[0], '2');
+
+      //entering 4 characters should move to the next cell.
+      //Unfortunately our code relies on focus() which doesn't seem to
+      //work in the testing environment. Instead listen to the event that will result
+      //in the cell to be changed
+      cells[0].addEventListener('px-entry-cell-move', function(evt) {
+
+        //fake the moving ourselves.............
+        cells[0]._handleBlur();
+debugger;
+        //make sure autocomplete happened
+        assert.equal(cells[0].dtWorkingCopy, 2222);
+        assert.equal(firstInput.value, 2222);
+        done();
+      })
+      fireKeyboardEvent(cells[0], '2');
+    });
   });
 };
+
+function fireKeyboardEvent(elem, key){
+  var evt = new CustomEvent('keydown',{detail:{'key':key,'keyIdentifier':key}});
+   elem.dispatchEvent(evt);
+}
